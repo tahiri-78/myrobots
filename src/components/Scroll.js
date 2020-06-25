2919ba75adaf989e6a8c4b56e4f919096afca76c
tahiri-78 <66792921+tahiri-78@users.noreply.github.com>
2020-06-25 01:30:56 +0100

    I've changed the css of scroll what do you think

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 const Scroll = (props) => {
   return (
     <div
-      style={{ overflow: "scroll", border: "4px solid black", height: "800px" }}
+      style={{ overflow: "scroll", border: "1px solid black", height: "800px" }}
     >
       {props.children}
     </div>

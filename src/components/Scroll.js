935b0096470b154c997383f26b5c18a718c5446a
tahiri-78 <66792921+tahiri-78@users.noreply.github.com>
2020-06-25 01:39:49 +0100

    changing color of border

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 const Scroll = (props) => {
   return (
     <div
-      style={{ overflow: "scroll", border: "4px solid black", height: "800px" }}
+      style={{ overflow: "scroll", border: "2px solid green", height: "800px" }}
     >
       {props.children}
     </div>

28f4861997ae9ce6817b076c5e479bdd3727b0be
tahiri-78 <66792921+tahiri-78@users.noreply.github.com>
2020-06-25 02:52:20 +0100

    some changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -34,7 +34,7 @@ class App extends Component {
       <h1>Loading</h1>
     ) : (
       <div className="tc">
-        <h1 className="f1">RoboFriends my master</h1>
+        <h1 className="f1">RoboFriends my mayrobots</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
